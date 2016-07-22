e56691ac99bbfb35cac4644f2eb7be677b9d9087
Carlos Russo <carlos.russo@webdetails.pt>
2016-07-22 18:39:03 +0100

    [WIP][BACKLOG-8276] Documentation for pentaho.util.Bitset

--- package-res/resources/web/pentaho/util/Bitset.js
+++ package-res/resources/web/pentaho/util/Bitset.js
@@ -6,23 +6,46 @@ define([
   return Base.extend(/** @lends pentaho.util.Bitset# */{
 
     constructor: function(bits) {
-      this._bits = bits != null ? bits : 0;
+      this.set(bits);
     },
 
+    /**
+     * Returns a value that indicates if all bits are cleared.
+     *
+     * @returns {boolean} `true` if no bits are set, `false` otherwise.
+     */
     get isClear (){
       return this.bits === 0;
     },
 
+    /**
+     * Returns an integer number with the bits currently set.
+     *
+     * @return {number} An integer with the bits currently set.
+     */
     get: function(){
       return this._bits;
     },
 
-    is: function(bits){
-      return this._bits === bits;
+    /**
+     * Tests if the current state is equal to a given mask.
+     *
+     * @param {!number} mask - An integer containing the bit mask to test.
+     * @return {boolean}
+     */
+    is: function(mask){
+      return this._bits === mask;
     },
 
-    set: function(bits) {
-      this._bits = (bits == null) ? ~0 : (this._bits | bits);
+    /**
+     * Sets the current state to a given mask.
+     *
+     * If a `nully` argument is provided, all bits are set.
+     *
+     * @param {number} mask - The bits to be set.
+     */
+    set: function(mask) {
+      this._bits = (mask == null) ? ~0 : (this._bits | mask);
     },
 
     /**
@@ -30,20 +53,21 @@ define([
      *
      * If a `nully` argument is provided, all bits are cleared.
      *
-     * @param {number} bits - The bits to be cleared.
+     * @param {number} mask - The bits to be cleared.
      */
-    clear: function(bits) {
-      this._bits = (bits == null) ? 0 : (this._bits & ~bits);
+    clear: function(mask) {
+      this._bits = (mask == null) ? 0 : (this._bits & ~mask);
     },
 
     /**
-     * Tests if the current state is a subset of a given mask.
+     * Returns a value that indicates if the current state is a subset of a given mask.
      *
      * Use this method to assert if no bits other than those described by the mask are
      * currently set.
      *
      * @param {!number} mask - An integer containing the bit mask to test.
-     * @return {boolean}
+     * @return {boolean} - `true` if the bits currently set are within the specified mask,
+     * `false` otherwise.
      */
     isSubsetOf: function(mask){
       return (this._bits | mask) === mask;
